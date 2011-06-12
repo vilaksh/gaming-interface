9a15ef90fd44f105ce284c5ec6704d2c65cc9ec3
Alok.gk05@gmail.com <Alok.gk05@gmail.com@e6efef27-6cf0-5011-92f1-9dcbd58285cd>
2011-06-12 16:07:51 +0000

    game engine

---
+++ branches/main/demo/players/ConsoleApplication1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string status;
+            //int[] x = { 0, 1, 0, 1, 2 };
+            //int[] y = { 0, 0, 2, 2, 1 };
+
+            int[] x = { 1, 2, 0, 2 };
+            int[] y = { 1, 0, 1, 2 };
+
+            int k = 0;
+            while (true)
+            {
+
+                status = System.Console.ReadLine();
+
+                if (!status.Equals("c"))
+                    break;
+
+                for (int i = 0; i < 9; i++)
+                {
+                    string pos = System.Console.ReadLine();
+                }
+
+                System.Console.WriteLine(x[k]);
+                if (k < 4)
+                    System.Console.WriteLine(y[k]);
+                k++;
+            }
+        }
+    }
+}
