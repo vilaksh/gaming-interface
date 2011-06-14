6cde05b9a0a21e562f32f7939f0799d286f13f87
viji133hp@gmail.com <viji133hp@gmail.com@e6efef27-6cf0-5011-92f1-9dcbd58285cd>
2011-06-14 17:47:00 +0000

    My player tested

--- branches/main/demo/players/PlayerViji.cpp
+++ branches/main/demo/players/PlayerViji.cpp
@@ -1,10 +1,79 @@
 #include<iostream>
 
 char board[3][3];
-
+char myCoin, oppCoin, empty;
+    
 int valuepos(int row, int col)
 {
-    return 0;
+    if(row == 1 && col == 1)
+     return 50;
+
+     int val = 0;
+
+     if((row+col)%2 ==0)
+      val++;
+     
+      for(int i=0;i<3;i++)
+      {
+           if(board[row][i] == empty || board[row][i] == myCoin)
+               val+=2;
+           else
+           {
+               val = val - i*2;
+               break;
+            }
+       }
+
+      for(int i=0;i<3;i++)
+      {
+          if(board[i][col] == empty || board[i][col] == myCoin)
+                val+=2;
+          else
+          {
+              val = val - i*2;
+              break;
+           }
+      }
+      
+      if( (row+col)%2 != 0)
+      {//std::cout<<"Value of"<<row<<" "<<col<<" "<<val<<"\n";
+       return val;}
+
+     // for winning major diagonal wise   
+   if(row == col)
+   {  
+    for(int i=0;i<3;i++)
+    {
+      if(board[i][i] == myCoin || board[i][i] == empty)
+       val+=2;
+
+       else
+       {
+           val = val-i*2;
+           break;
+       }
+    }
+    }
+    else
+    {   
+
+     // for winning minor diagonal wise     
+    for(int i=0;i<3;i++)
+    {
+      if(board[i][2-i] == myCoin || board[i][2-i] == empty)
+       val+=2;
+
+       else
+       {
+            val = val - i*2;
+            break;
+        }
+    }  
+    }
+    
+    //std::cout<<"Value of"<<row<<" "<<col<<" "<<val<<"\n";
+    return val;
+      
 }
 
 bool willwin(char coin, int row, int col)
@@ -15,7 +84,10 @@ bool willwin(char coin, int row, int col)
      count = 0;
      for(int i=0;i<3;i++)
       if(board[row][i] == coin)
-       count ++;
+        count++;
+       else if(board[row][i] != empty)
+        {count = 0; break;}
+       
        
      if(count == 2)
           return true;
@@ -24,39 +96,51 @@ bool willwin(char coin, int row, int col)
      count = 0;
      for(int i=0;i<3;i++)
       if(board[i][col] == coin)
-       count ++;
+        count++;
+       else if(board[i][col] != empty)
+        {count = 0; break;}
+       
        
      if(count == 2)
-      return true;
+          return true;
 
       // verify if the cell falls on a diagonal
-      if(row+col %2 != 0)
+      if( (row+col)%2 != 0)
        return false;
 
-     // for winning major diagonal wise     
+     // for winning major diagonal wise   
+     if(row == col)
+     {  
      count = 0;
      for(int i=0;i<3;i++)
       if(board[i][i] == coin)
-       count ++;
+        count++;
+       else if(board[i][i] != empty)
+        {count = 0; break;}
+       
        
      if(count == 2)
-      return true;
-
+          return true;
+     }
+     else
+     {
      // for winning minor diagonal wise     
      count = 0;
      for(int i=0;i<3;i++)
       if(board[i][2-i] == coin)
-       count ++;
+        count++;
+       else if(board[i][2-i] != empty)
+        {count = 0; break;}
+       
        
      if(count == 2)
-      return true;
- 
+          return true;
+    }
       return false;
 }
 
 int main()
 {
-    char myCoin, oppCoin, empty;
     bool gameover = false;
     char b;
     
@@ -65,42 +149,54 @@ int main()
     while(!gameover)
     {
         std::cin>>b;
-        if(b == 'W' || b == 'L')
+        if(b == 'W' || b == 'L' || b == 'w' || b == 'l')
         {
             gameover = true;
             continue;        
         }
                 
-        std::cin>>b;
         for(int i=0;i<3;i++)
          for(int j=0;j<3;j++)
          {
+                 std::cin>>b;        
                  board[i][j] = b;
-                 std::cin>>b;
          }
         
-        int maxvalue = 0;
+        int maxvalue = -1;
+        bool flag = false;
         int rrow, rcol;
         for(int i=0;i<3;i++)
-        for(int j=0;j<3;j++)
         {
-            if(willwin(myCoin, i, j))
+            for(int j=0;j<3;j++)
             {
-           std::cout<<i<<j;
-           break;
-           } 
-           else if(willwin(oppCoin, i, j))
-           {
-                std::cout<<i<<j;
-                break;
+                if(board[i][j] != empty)
+                 continue;
+    
+                if(willwin(myCoin, i, j))
+                {
+                   //std::cout<<"my win\n"<<i<<"\n"<<j<<"\n";
+                   flag = true;
+                   break;
+               } 
+               else if(willwin(oppCoin, i, j))
+               {
+                    //std::cout<<"opp win\n"<<i<<"\n"<<j<<"\n";
+                    flag = true;
+                    break;
                 }
                 else if(valuepos(i,j)>maxvalue)
                 {
                      rrow = i;
                      rcol = j;
-                     maxvalue = valuepos(i,j);
-                     }
-        }                                
+                     maxvalue = valuepos(i,j);                     
+                }
+            }
+            if(flag)
+             break; 
+        }                               
+        if(!flag)
+             std::cout<<rrow<<"\n"<<rcol<<"\n";
+                        
         
     }
 }
